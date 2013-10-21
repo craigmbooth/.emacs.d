c5c79bc0a6d90ceb98dd1085103cafa96429aec1
Craig Booth <craigmbooth@gmail.com>
2013-10-21 18:21:03 -0500

    Added ipython integration

--- cmbmacs/cmb-python.el
+++ cmbmacs/cmb-python.el
@@ -18,3 +18,23 @@
 ;;   (lambda()
 ;;      flyspell-prog-mode 1))
 
+(require 'python-mode)
+
+;; use IPython
+;; C-c ! opens a python frame
+;; C-c C-c executes the current buffer
+;; C-c | executes the current region
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+  '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+; don't split windows
+(setq py-split-windows-on-execute-p nil)
+; try to automagically figure out indentation
+(setq py-smart-indentation t)
