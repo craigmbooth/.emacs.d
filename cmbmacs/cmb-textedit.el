e23cbab44bc5a7fc2767f07a43889e96c1831010
craigmbooth <craigmbooth@gmail.com>
2013-10-24 12:23:32 -0500

    Ensure correct abbrev file is used

--- cmbmacs/cmb-textedit.el
+++ cmbmacs/cmb-textedit.el
@@ -1,10 +1,12 @@
 ;; Definitions to be used when doing plaintext editing
 
-;;Spellcheck latex documents
+;; Spellcheck latex documents
 (add-hook 'latex-mode-hook
   (lambda()
     (flyspell-mode 1)))
 
+;; Activate abbrev mode, diminish it and ensure the correct abbrev file is used
+(setq abbrev-file-name "~/.emacs.d/abbrev_defs")
 (setq default-abbrev-mode t)
 (diminish 'abbrev-mode)
 
