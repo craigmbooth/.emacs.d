e5e5a5d8e72f3ebc6a2c824c858f634e012fb853
Craig Booth <craigmbooth@gmail.com>
2013-10-25 10:18:19 -0500

    Add details to fringe, more highlight parens

--- cmbmacs/cmb-appearance.el
+++ cmbmacs/cmb-appearance.el
@@ -1,41 +1,44 @@
-;;Zenburn theme with electric pink cursor
+;; Zenburn theme with electric pink cursor
 (when (> emacs-major-version 23)
 (progn 
   (load-theme 'zenburn t)
   (add-to-list 'default-frame-alist '(cursor-color . "#f00bac"))
 ))
 
-;;Stop the cursor from blinking
+;; Stop the cursor from blinking
 (blink-cursor-mode -1)
 
-;;Always show column number
+;; Always show column number
 (column-number-mode 1)
 
-;;Highlight the current buffer
+;; Highlight the current buffer
 (auto-dim-other-buffers-mode 1)
 
-
-
-;;Show line numbers to left of screen
+;; Show line numbers to left of screen
 (global-linum-mode t)
 
-;;Make the cursor stretch when it's on a wide-char like a tab:
+;; Change appearance of the fringes to show extent of buffer
+(setq-default indicate-buffer-boundaries 'left)
+(setq-default indicate-empty-lines +1)
+
+;; Make the cursor stretch when it's on a wide-char like a tab:
 (setq x-stretch-cursor t)
 
-;;When opening two files with the same name, don't name
-;;the second one file<2>, instead prepend the directory
+;; When opening two files with the same name, don't name
+;; the second one file<2>, instead prepend the directory
 (require 'uniquify)
   (setq uniquify-buffer-name-style 'forward)
 
-;;highlight matching parens
+;; highlight matching parens
 (show-paren-mode 1)
+(setq show-paren-style 'expression)
 
-;;put emacs: [buffer name] in frame title
+;; put emacs: [buffer name] in frame title
 (setq frame-title-format
   '("emacs: " (buffer-file-name "%f"
     (dired-directory dired-directory "%b"))))
 
-;;Let [f11] toggle fullscreen
+;; Let [f11] toggle fullscreen
 (defun toggle-fullscreen ()
   "Toggle full screen on X11"
   (interactive)
